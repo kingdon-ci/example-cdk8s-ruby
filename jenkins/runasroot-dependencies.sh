029fd2bbe7ecceffd9c8f4abfc3ec21e1ba251c7
Kingdon Barrett <kingdon@weave.works>
2021-01-29 14:05:53 -0500

    jenkins/runasroot-*

---
+++ jenkins/runasroot-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /etc/profile.d/rvm.sh
+set -euo pipefail
+
+# Install any desirable packages here
+apt-get update && apt-get install -y --no-install-recommends \
+  manpages vim \
+  build-essential rsync \
+  tzdata
+
+# # Upgrade, install nodejs, yarn (in the RVM context)
+apt-get upgrade -y --no-install-recommends
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -o- -L https://yarnpkg.com/install.sh | bash
+
+# Clean up
+apt-get clean
+rm -rf /var/lib/apt/lists/*
---
+++ jenkins/runasroot-jenkins-dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+apt-get update
+
+apt-get install -y \
+  netcat-openbsd \
+  build-essential \
+  # xvfb \
+  # libqt5webkit5-dev \
+  # qt5-default \
+  libpq-dev \
+  libyaml-dev \
+  libreadline6-dev \
+  zlib1g-dev \
+  libssl-dev \
+  libyaml-dev \
+  libsqlite3-dev \
+  sqlite3 \
+  libgdbm-dev \
+  libncurses5-dev \
+  libffi-dev \
+  imagemagick \
+  wget
+  # libxss1 \
+  # libappindicator3-1 \
+  # libindicator7 \
+  # libgtk-3-0 \
+  # fonts-liberation \
+  # xdg-utils \
+  # gconf-service \
+  # libgconf-2-4 \
+  # lsb-release \
+  # libasound2 \
+  # libnss3 \
+  # libnspr4
+
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# dpkg -i google-chrome-stable_current_amd64.deb
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
