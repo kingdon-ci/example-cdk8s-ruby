15631877cd609064ab7ce8bc3357d4f1c51dce0c
Kingdon Barrett <kingdon@weave.works>
2021-02-01 14:36:46 -0500

    does this get applied ?

--- synths/main.ts
+++ synths/main.ts
@@ -5,8 +5,36 @@ export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    // define resources here
+    const label = { app: 'hello-k8s' };
 
+    new KubeService(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
   }
 }
 
