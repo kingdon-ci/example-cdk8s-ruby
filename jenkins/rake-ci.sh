3b956de96f2203b951b408768eab9b566ea55061
Kingdon Barrett <kingdon@weave.works>
2021-02-01 14:20:55 -0500

    rebase spike-development-3
    $GIT_REPO - variable for development purposes
    
    it will be handy to be able to swap out this git repository easily from
    here in the script, so let's make it a variable parameter in the script.
    
    Was on the fence about running: cdk8s import k8s inside of CI,
    opted to run it upstream intead.
    
    npm run compile will succeed as long as k8s.ts is included
    tests are used to ensure smoke-test readiness but are not comprehensive
    
    * add package-lock.json - there is an error from CI about npm version
    
    * Generally do not commit synths/*.js
    (except for synths/jest.config.js, which is full of test configuration)
    
    * do not include compilation targets
    Generated by npm run compile and they should not be committed in CI
    
    * do not save __snapshots__/*.test.ts.snap
    
    I am not sure how snapshots work but they are recommended as a test
    strategy in the distro files from cdk8s.  I do not know how jest works
    outside of basic things, will use it to write some tests maybe, or skip
    it. (It would be better to test this with something comprehensive before
    it is applied to the cluster, but maybe kustomization's health checking
    is good enough to avoid a complex suite of tests upstream...)
    
    https://openconstructfoundation.org/testing-cdk8s/
    
    So we can see a problem in our generated output thanks to test failures,
    the namespace has not been mentioned in the re-hydrated yaml from cdk8s.
    
    Let's add a test that asserts we include the namespace, and include it.
    
    * run the tests in CI
    this should stop synths commit from being generated in event that it
    failed the test (this is imperfect, but it's a useful mitigation)
    
    * do not use snapshots for now

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-# source /etc/profile.d/rvm.sh
-# rvm ${RUBY}@testing
-# 
-# bundle check
-# bundle exec rake ci
+GIT_REPO=git@github.com:kingdonb/example-cdk8s-ruby.git
+# GIT_REPO=https://github.com/kingdon-ci/example-cdk8s-ruby
 
 # run cdk8s to hydrate yamls in dist/
 pushd synths
 npm install
+
+# run compile and test (fail the script and exit if test fails)
+set -ex
+npm run compile
+npm run test
+set +ex
+
+# execute "synth" and make our synthetized yamls
 cdk8s synth
 
 # verbose
 set -x
 
+# add the SSH key mounted from secret, and clone git branch 'synths'
 ssh-add ~/.ssh/id_ed25519_flux
-git clone git@github.com:kingdonb/example-cdk8s-ruby.git -b synths git-dist
+git clone $GIT_REPO -b synths git-dist
 cd git-dist
 
 # prune (dist is only generating one file anyway)
 mv -f ../dist/synths.k8s.yaml ./
 
-# sleep 6000 
 # generate a new commit on synths branch and push
+# FIXME: branches other than (whatever) should not result in a commit
 git add synths.k8s.yaml
 git config --global user.email "kingdon-ci@nerdland.info"
 git config --global user.name "kingdon-ci Robot (Jenkins)"
