211210466286594e82d9c813d34a26be06c87a71
Kingdon Barrett <kingdon@weave.works>
2021-02-01 09:24:06 -0500

    (red) run cdk8s and make a git commit, push it up
    I am expecting this to fail at `git push origin synths` because I am not
    passing in any git credentials that can write back to the repo (not yet)

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -5,3 +5,29 @@ rvm ${RUBY}@testing
 
 bundle check
 bundle exec rake ci
+
+# run cdk8s to hydrate yamls in dist/
+pushd synths
+cdk8s synth
+
+# strict bash
+set -euo pipefail
+# verbose
+set -x
+
+# harvest .git for use with synths/dist content
+mkdir git-dist
+rsync -a --delete ../.git/ git-dist/.git
+cd git-dist
+
+# prune (dist is only generating one file anyway)
+git fetch
+git checkout --track origin/synths
+mv -f ../dist/synths.k8s.yaml ./
+
+# generate a new commit on synths branch and push
+git commit -m"built synth.k8s from $GIT_COMMIT" synths.k8s.yaml
+git push origin synths
+
+# all done
+popd
