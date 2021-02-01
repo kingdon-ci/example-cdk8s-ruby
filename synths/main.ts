743885a9eb10c799058228c7c095b41834da5a67
Kingdon Barrett <kingdon@weave.works>
2021-02-01 14:20:55 -0500

    rebase spike-development-2
    for development purposes
    
    it will be handy to be able to swap out this git repository easily from
    here in the script, so let's make it a variable parameter in the script.
    
    $GIT_REPO
    
    cdk8s import k8s && npm run compile
    (make sure that k8s.ts is included for ci)
    
    do not exclude imports/ from docker build
    
    Should probably exclude .js and other expanded forms of the typescript
    from git commits, but for now naively I have excluded too much. From
    imports, imports/k8s.ts file at least must be included for compilation
    using 'npm run compile' to succeed.

--- synths/main.ts
+++ synths/main.ts
@@ -1,12 +1,42 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
 
+import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    // define resources here
+    const label = { app: 'hello-k8s' };
 
+    new KubeService(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
   }
 }
 
