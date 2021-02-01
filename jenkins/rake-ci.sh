5a5c9ff3210f50b67d2b84b764a849027dcbb354
Kingdon Barrett <kingdon@weave.works>
2021-02-01 14:20:55 -0500

    thankfully when nothing has changed,
    it does not result in a new commit, or a push event

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# source /etc/profile.d/rvm.sh
-# rvm ${RUBY}@testing
-# 
-# bundle check
-# bundle exec rake ci
-
 # run cdk8s to hydrate yamls in dist/
 pushd synths
 npm install
@@ -21,12 +15,12 @@ cd git-dist
 # prune (dist is only generating one file anyway)
 mv -f ../dist/synths.k8s.yaml ./
 
-# sleep 6000 
 # generate a new commit on synths branch and push
 git add synths.k8s.yaml
 git config --global user.email "kingdon-ci@nerdland.info"
 git config --global user.name "kingdon-ci Robot (Jenkins)"
 git commit -m"built synth.k8s from $GIT_COMMIT" synths.k8s.yaml
+sleep 6000
 git push origin synths
 
 # all done
