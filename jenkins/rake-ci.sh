5d6bf9c4a442c62e7af88d6dd53678ef45aec526
Kingdon Barrett <kingdon@weave.works>
2021-02-01 13:29:27 -0500

    WIP - work with mounted SSH key volume from secret

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -8,6 +8,7 @@ bundle exec rake ci
 
 # run cdk8s to hydrate yamls in dist/
 pushd synths
+npm install
 cdk8s synth
 
 # strict bash
@@ -15,11 +16,16 @@ set -euo pipefail
 # verbose
 set -x
 
-# harvest .git for use with synths/dist content
-mkdir git-dist
+# # harvest .git for use with synths/dist content
+# mkdir git-dist
+# # rsync -a --delete ../.git/ git-dist/.git
+# cd git-dist
+
+eval `ssh-agent`; ssh-add ~/.ssh/id_ed25519_flux
+
+git clone git@github.com:kingdonb/example-cdk8s-ruby.git -b synths
+
 sleep 6000
-rsync -a --delete ../.git/ git-dist/.git
-cd git-dist
 
 # prune (dist is only generating one file anyway)
 git fetch
