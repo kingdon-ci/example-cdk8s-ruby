6cc17c0f2bbb417623977f17364881050b1dd759
Kingdon Barrett <kingdon@weave.works>
2021-02-01 13:49:32 -0500

    WIP: this errors because rvm is loaded
    don't need rvm, this project still doesn't have any ruby component

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-source /etc/profile.d/rvm.sh
-rvm ${RUBY}@testing
-
-bundle check
-bundle exec rake ci
+# source /etc/profile.d/rvm.sh
+# rvm ${RUBY}@testing
+# 
+# bundle check
+# bundle exec rake ci
 
 # run cdk8s to hydrate yamls in dist/
 pushd synths
 npm install
 cdk8s synth
 
-# strict bash
-set -euo pipefail
 # verbose
 set -x
 
@@ -23,7 +21,7 @@ cd git-dist
 # prune (dist is only generating one file anyway)
 mv -f ../dist/synths.k8s.yaml ./
 
-sleep 6000 
+# sleep 6000 
 # generate a new commit on synths branch and push
 git commit -m"built synth.k8s from $GIT_COMMIT" synths.k8s.yaml
 git push origin synths
