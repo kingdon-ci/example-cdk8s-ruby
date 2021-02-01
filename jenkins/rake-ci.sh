f01c0b18424d60e5879a4cd537a0bd20ca2b9dbc
Kingdon Barrett <kingdon@weave.works>
2021-02-01 13:56:42 -0500

    WIP: make sure you 'git add' to stage the file
    otherwise git commit may not be able to find it in the stage

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -23,6 +23,7 @@ mv -f ../dist/synths.k8s.yaml ./
 
 sleep 6000 
 # generate a new commit on synths branch and push
+git add synths.k8s.yaml
 git commit -m"built synth.k8s from $GIT_COMMIT" synths.k8s.yaml
 git push origin synths
 
