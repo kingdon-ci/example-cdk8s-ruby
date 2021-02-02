293605b3493f226c6fdbb18d20265712af5dcfaf
Kingdon Barrett <kingdon@weave.works>
2021-02-02 10:05:40 -0500

    add okteto.yml (#7)
    cdk8s init typescript-app
    
    (first: npm install -g cdk8s)
    
    "cdk8s import k8s" generate from inside of CI, or committed in the repo?
    opted to run it client-side.
    
    tests are not comprehensive (add kubeval in a future iteration)
    
    * add package-lock.json - there is an error from CI about npm version
    Our upstream base image is using NodeJS 14, which is the LTS release
    
    * Generally do not commit synths/*.js
    (except for synths/jest.config.js, which is full of test configuration)
    
    * do not include compilation targets (*.js)
    Generated by npm run compile and they should not be committed in CI
    
    * do not save __snapshots__/*.test.ts.snap
    
    I am not sure how to use snapshots as an effective test strategy that
    scales, so I will not use them.
    
    (It is better to add health checks, run kubeval before generating a
    commit, this way the tool can help you avoid a production incident.)
    
    https://openconstructfoundation.org/testing-cdk8s/
    If there are tests or validation in your chosen framework (jsonnet,
    cdk8s, etc.) then of course you should also run them, and be sure that
    if they fail, a commit is not generated.
    
    We run "npm run compile" and "npm run test" with set -e so the build
    will fail, and script will exit without generating a commit if compile
    fails, or if a test does not pass. This will prevent downstream failure
    based on partial apply or inconsistent state.
    
    So we can see a problem in our generated output thanks to test failures,
    the namespace has not been mentioned in the re-hydrated yaml from cdk8s.
    
    Let's add a test that asserts we include the namespace, and include it.
    
    ...
    
    * run the tests in CI
    this should stop synths commit from being generated in event that it
    failed the test (this is imperfect, but it's a useful mitigation)
    
    * do not use snapshots for now
    
    * show how to update a deploy through cdk8s synth

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-source /etc/profile.d/rvm.sh
-rvm ${RUBY}@testing
+GIT_REPO=git@github.com:kingdonb/example-cdk8s-ruby.git
+# GIT_REPO=https://github.com/kingdon-ci/example-cdk8s-ruby
 
-bundle check
-bundle exec rake ci
+# run cdk8s to hydrate yamls in dist/
+pushd synths
+npm install
+
+# run compile and test (fail the script and exit if test fails)
+set -ex
+npm run compile
+npm run test
+set +ex
+
+# execute "synth" and make our synthetized yamls
+cdk8s synth
+
+# verbose
+set -x
+
+# add the SSH key mounted from secret, and clone git branch 'synths'
+ssh-add ~/.ssh/id_ed25519_flux
+git clone $GIT_REPO -b synths git-dist
+cd git-dist
+
+# prune (dist is only generating one file anyway)
+mv -f ../dist/synths.k8s.yaml ./
+
+# generate a new commit on synths branch and push
+# FIXME: branches other than (whatever) should not result in a commit
+git add synths.k8s.yaml
+git config --global user.email "kingdon-ci@nerdland.info"
+git config --global user.name "kingdon-ci Robot (Jenkins)"
+git commit -m"built synth.k8s from $GIT_COMMIT" synths.k8s.yaml
+git push origin synths
+
+# all done
+popd
